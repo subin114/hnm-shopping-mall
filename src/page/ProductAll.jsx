fce5d0256ff43416be3a3a1ffea367fff3e61f08
subin114 <bin7851@naver.com>
2024-05-16 00:30:30 +0900

    라우터 세팅

---
+++ src/page/ProductAll.jsx
@@ -0,0 +1,5 @@
+const ProductAll = () => {
+  return <div>전체 상품 페이지</div>;
+};
+
+export default ProductAll;
---
+++ src/page/ProductDetail.jsx
@@ -0,0 +1,5 @@
+const ProductDetail = () => {
+  return <div>상품 상세 페이지</div>;
+};
+
+export default ProductDetail;
